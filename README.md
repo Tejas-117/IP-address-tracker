1f120a20526c27d5e9e0d4c1272ac9cbf5c474f7
Tejas <tejasftw117@gmail.com>
2021-05-29 20:51:48 +0530

    Initial commit of final solution

---
+++ README.md
@@ -0,0 +1,58 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Links
+
+- [Solution URL](https://your-solution-url.com)
+- [Live Site URL](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Desktop-first workflow
+
+### What I learned
+
+This project helped me sharpen my skills in using APIs :)
+
+### Continued development
+
+For know, I have used Javascript 'fetch' alone for making API calls, I would like to update it using Javascript 'async', 'await';
+
+### Useful resources
+
+- [Geo Ipify](https://geo.ipify.org/) - This helped me to get location based on IP addresses. I really liked this pattern and will use it going forward.
+- [LeafLet JS](https://leafletjs.com/) - This is an amazing tool which helped me to render maps based on IP latitude and longitude.
+
+
+## Author
+
+- Website - [Tejas](https://github.com/Tejas-117)
+- Frontend Mentor - [@Tejas-117](https://www.frontendmentor.io/profile/Tejas-117)
---
+++ js/script.js
@@ -0,0 +1,69 @@
+const search = document.getElementById('search');
+
+// fetching data based on IP;
+const API_KEY = config.API_KEY;
+let IP = '';
+
+const getUserInfo = (IP) => {
+   if(validateIP(search.value)){
+      IP = search.value;//if format is correct OR empty;
+   }
+   let URL = `https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${IP}`;
+      fetch(URL)
+      .then(res => res.json())
+      .then(data => {
+         displayUserInfo(data);
+         displayMap(data);
+      });   
+}
+getUserInfo('');//default empty IP address;
+
+function validateIP(IP){//function to validate the user IP input;
+   var ipformat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+     if(IP.match(ipformat) || IP==''){
+        return true;
+     }
+     else {
+        alert("Wrong or invalid IP, please try again");
+        return false;
+     }
+  }
+
+// info to be displayed;
+const userIP = document.getElementById('ip');
+const userLoaction = document.getElementById('location');
+const userTimeZone = document.getElementById('timezone');
+const userISP = document.getElementById('isp');
+
+const displayUserInfo = (data) => {
+   userIP.innerText = data.ip;
+   userLoaction.innerText = `${data.location.city},${data.location.region}, ${data.location.country}`;
+   userTimeZone.innerText = `UTC${data.location.timezone}`;
+   userISP.innerText = data.isp;
+}
+
+// displaying the map
+const accessToken = config.ACCESS_TOKEN;
+let myMap = L.map('mapid');
+
+const displayMap = (data) => {
+   let lat = data.location.lat;
+   let lng = data.location.lng;
+
+   // rendering appropriate location;
+   myMap.setView([lat,lng], 13);
+
+   // displaying marker on the map;
+   let myIcon = L.icon({
+      iconUrl: "./images/icon-location.svg",
+      iconSize: [46,56],
+   });
+   const marker = L.marker([lat,lng], {icon: myIcon}).addTo(myMap);
+
+   L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${accessToken}`, {
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+   }).addTo(myMap);
+}
