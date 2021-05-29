ca99af8f6acfc38d2187fac6e9ee293d294a67c8
Tejas <tejasftw117@gmail.com>
2021-05-29 20:57:31 +0530

    final solution

---
+++ js/config.js
@@ -0,0 +1,4 @@
+let config = {
+   'API_KEY': 'at_q5SUUtkkr61jzxGAzhVeZF7OONVY7',
+   'ACCESS_TOKEN': 'pk.eyJ1IjoiYWthLXRlamFzIiwiYSI6ImNrcDlvemJ3aTA2ZWQydmxraWJ4a3o1enYifQ.vO2rvKbQ_gQkr18Bmp8ncQ'
+}
